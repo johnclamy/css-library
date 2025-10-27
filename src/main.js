9ce3d1b533010d2812259f98163725df62b1b49f
johnclamy <johnclamy@gmail.com>
2025-10-27 08:15:14 +0000

    setup the html for the single page site

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+import scones from './scones.jpg'
+import logo from '/SOC-Logo.png'
+
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
