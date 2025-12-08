8c1db97432751f8e5eeb3811ee5284badae9b6ba
johnclamy <johnclamy@gmail.com>
2025-12-08 20:52:39 +0000

    pass down the home page title from view function to index.html templates

--- root_app/views.py
+++ root_app/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'root_app/index.html')
+    template_data = {}
+    template_data['page_title'] = 'Welcome to Riverside Pet Store | Home Page'
+
+    return render(
+        request,
+        'root_app/index.html',
+        { 'template_data': template_data }
+    )
 
 
 def about_page(request):
